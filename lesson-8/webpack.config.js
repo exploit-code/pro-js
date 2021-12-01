523c92cf441ef7f90da03120d9f9fdc86c4c0d5e
Borushko Alexander <o_xx@list.ru>
2021-12-02 01:45:52 +0300

    lesson-8

---
+++ lesson-8/webpack.config.js
@@ -0,0 +1,35 @@
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['./public/js/main.js', './public/js/CartComponent.js', './public/js/ErrorComponent.js', './public/js/SearchComponent.js', './public/js/ProductComponent.js'],
+    output: {
+        filename: "./build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new VueLoaderPlugin(),
+    ]
+}
